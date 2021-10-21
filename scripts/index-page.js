20e1e068e91fb065aa433a69aa039f61c536178b
Eddie <ed201112@gmail.com>
2021-10-20 23:00:05 -0700

    post comment

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -35,10 +35,9 @@ const createElement = (type) => (classes) => {
 const createDiv = createElement("div");
 const createParagraph = createElement("p");
 
-// displays all comments 
-const displayComments = (comments) => {
-    comments.forEach((item)=>{
-        const commentBlock = createDiv("comments__block");
+// displays single comment
+const displaySingleComment = (newComment) => {
+    const commentBlock = createDiv("comments__block");
         allComments.appendChild(commentBlock);
         
         const avatar = createDiv(["comments__avatar", "comments__avatar--empty"]);
@@ -48,40 +47,99 @@ const displayComments = (comments) => {
         commentBlock.appendChild(comment);
     
         const name = createDiv("comments__comment-name");
-        name.innerText = item["name"];
+        name.innerText = newComment["name"];
         const time = createDiv( "comments__comment-time");
-        time.innerText = item["time"];
+        const timeStamp = newComment["time"];
+        time.innerText = timeStampToTime(timeStamp);
         const text = createParagraph("comments__comment-text");
-        text.innerText = item["text"];
+        text.innerText = newComment["text"];
         comment.append(name, time, text);
     
         const divider = document.createElement("hr");
         divider.classList.add("comments__divider");
         allComments.appendChild(divider);
-    });
-};
+}
 
 // add and display a new comment
 const displayComment = (newComment)=>{
     comments.unshift(newComment);
-    displayComments(comments);
+    displayAllComments(comments);
 };
-  
+
+// display all comments
+const displayAllComments = (comments) => {
+    comments.sort(compare);
+    comments.forEach((comment)=>{
+        displaySingleComment(comment);
+    });
+}
+
+// get comments from API
+const getComments = (url, comments) => {
+    axios.get(url)
+    .then(response => {
+        response.data.forEach(item => {
+            let newComment = {
+                name: item.name,
+                time: item.timestamp,
+                text: item.comment
+            };
+            console.log(item);
+            comments.push(newComment);
+        });
+    })
+    .then(()=>displayAllComments(comments))
+    .catch(error => console.error(error));
+};
+
+// post comment to server
+const postComment = (url, newComment, comments) => {
+    axios.post(url, newComment)
+    .then(response => {
+        let comment = {
+            name: response.data.name,
+            time: response.data.timestamp,
+            text: response.data.comment
+        };
+        comments.unshift(comment);
+    })
+    .then(()=>displayAllComments(comments))
+    .catch(error => console.error(error));
+};
+
+// delete comment by id
+const deleteComment = (url, id, key) => {
+    axios.delete(url+'/'+id+key)
+    .then((response => console.log(response)))
+    .catch(error => console.error(error));
+};
+
+// compare comment by time
+const compare = ( a, b ) => {
+    if ( a.time > b.time ){
+      return -1;
+    }
+    if ( a.time < b.time ){
+      return 1;
+    }
+    return 0;
+  };
+
+// get time from timestamp
+const timeStampToTime = (timeStamp) => {
+    const date = new Date(timeStamp);
+    const time = `${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`;
+    return time;
+};
+
 // main
 
 // read comments from API
 const comments = [];
-const url = "https://project-1-api.herokuapp.com/comments" + apiKey;
-axios.get(url).then(response => {
-    response.data.forEach(item => {
-        let newComment = {
-            name: item.name,
-            time: item.timestamp,
-            text: item.comment
-        };
-        comments.push(newComment);
-    });
-}).then(()=>displayComments(comments));
+const urlNoKey = "https://project-1-api.herokuapp.com/comments";
+const url = urlNoKey + apiKey;
+getComments(url, comments);
+
 
 const commentSection = document.querySelector(".comments");
 const allComments = createDiv("comments__all");
@@ -94,15 +152,12 @@ form.addEventListener("submit", (event)=>{
     document.querySelector(".comments__all").innerHTML = "";
     const name = event.target.name.value;
     const text = event.target.comment.value;
-    const date = new Date();
-    const time = `${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`;
     let newComment = {
-        name: name,
-        time: time,
-        text: text
+        "name": name,
+        "comment": text
     };
-    displayComment(newComment);
-    document.getElementById('form').reset();
+    postComment(url, newComment, comments);
+    form.reset();
 });
 
 
